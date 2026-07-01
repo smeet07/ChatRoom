a9a1b73623042ad37e145459b223b0c29db9bf2f
Smeet Nagda <smeetnagda@Smeets-MacBook-Air-3.local>
2026-07-01 14:26:33 -0400

    join different rooms

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,10 +1,41 @@
 package handlers
-import "github.com/gofiber/fiber/v2"
+
+import (
+	"net/url"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
 type AppHandler struct{}
-func NewAppHandler() *AppHandler{
+
+func NewAppHandler() *AppHandler {
 	return &AppHandler{}
 }
-func( a *AppHandler) HandleGetIndex(ctx *fiber.Ctx) error{
-	context:=fiber.Map{}
-	return ctx.Render("index",context)
+func (a *AppHandler) HandleGetIndex(ctx *fiber.Ctx) error {
+	context := fiber.Map{}
+	return ctx.Render("index", context)
+}
+
+func (a *AppHandler) HandleJoinRoom(ctx *fiber.Ctx) error {
+	user := strings.TrimSpace(ctx.FormValue("user"))
+	roomCode := strings.TrimSpace(ctx.FormValue("room"))
+	if user == "" || roomCode == "" {
+		return ctx.Redirect("/")
+	}
+
+	return ctx.Redirect("/room/" + url.PathEscape(roomCode) + "?user=" + url.QueryEscape(user))
+}
+
+func (a *AppHandler) HandleGetRoom(ctx *fiber.Ctx) error {
+	roomCode := strings.TrimSpace(ctx.Params("code"))
+	user := strings.TrimSpace(ctx.Query("user"))
+	if roomCode == "" || user == "" {
+		return ctx.Redirect("/")
+	}
+
+	return ctx.Render("room", fiber.Map{
+		"RoomCode": roomCode,
+		"User":     user,
+	})
 }
--- main.go
+++ main.go
@@ -1,28 +1,32 @@
 package main
+
 import (
+	"github.com/Smeet07/ChatAppGo/handlers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
-	"github.com/Smeet07/ChatAppGo/handlers"
 	"github.com/gofiber/websocket/v2"
 )
-func main(){
-	viewsEngine:=html.New("./views",".html")
-	app:=fiber.New(fiber.Config{
+
+func main() {
+	viewsEngine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
 		Views: viewsEngine,
 	})
-	app.Static("/static","./static")
-	app.Get("/ping",func(c *fiber.Ctx) error{
+	app.Static("/static", "./static")
+	app.Get("/ping", func(c *fiber.Ctx) error {
 		return c.SendString("Welcome to fiber")
 	})
 	appHandler := handlers.NewAppHandler()
-    // Add appHandler routes
-    app.Get("/", appHandler.HandleGetIndex)
+	// Add appHandler routes
+	app.Get("/", appHandler.HandleGetIndex)
+	app.Post("/join", appHandler.HandleJoinRoom)
+	app.Get("/room/:code", appHandler.HandleGetRoom)
 	server := NewWebSocket()
-    app.Get("/ws", websocket.New(func(ctx *websocket.Conn) {
-        server.HandleWebSocket(ctx)
-    }))
+	app.Get("/ws/:code", websocket.New(func(ctx *websocket.Conn) {
+		server.HandleWebSocket(ctx, ctx.Params("code"))
+	}))
 
-    go server.HandleMessages()
+	go server.HandleMessages()
 	app.Listen(":3000")
 
 }
